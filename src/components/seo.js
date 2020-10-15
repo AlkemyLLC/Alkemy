7d365979da20303850019ad62ae212363d4807a7
jferragut <31458614+jferragut@users.noreply.github.com>
2020-10-15 00:48:35 -0400

    add font-preload and gtm plugins

--- src/components/seo.js
+++ src/components/seo.js
@@ -150,21 +150,6 @@ function SEO({
             <TypographyStyle typography={typography} />
             <GoogleFont typography={typography} />
 
-            {/* Google Tag Manager */}
-            <script>{`
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({
-                  'gtm.start':new Date().getTime(),event:'gtm.js'
-                });
-                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TRL98WJ');
-            `}</script>
-            {/* End Google Tag Manager */}
-
             <script defer type="application/ld+json">
                 {`{
                 "@context":"https://schema.org",
