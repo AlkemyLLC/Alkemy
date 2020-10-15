3daf9e0d635ffac05fe2cf0497bf49e5d8f4f80e
jferragut <31458614+jferragut@users.noreply.github.com>
2020-10-15 17:47:48 -0400

    create blog category bar and start working on layout

---
+++ src/components/BlogCategoryBar.jsx
@@ -0,0 +1,37 @@
+import React,{useState} from "react";
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
+import { uniq } from "lodash";
+import { Nav, NavItem, NavLink } from "reactstrap";
+
+const BlogCategoryBar = (props) => {
+    const { categories, onSelectCategory, defaultSelected } = props;
+    const [selection, setSelection] = useState(defaultSelected);
+
+    const getPills =
+        categories &&
+        categories.length > 0 &&
+        categories.map(item => (
+            <NavItem
+                key={item.value}
+                onClick={e => {
+                    setSelection(item.value);
+                    onSelectCategory(item.value)
+                }}
+            >
+                <NavLink href="#" active={item.value === selection}>
+                    {item.label}
+                </NavLink>
+            </NavItem>
+        ));
+
+    return <Nav pills>{getPills}</Nav>;
+};
+
+BlogCategoryBar.propTypes = {
+    onSelectCategory: PropTypes.func,
+    categories: PropTypes.array,
+    defaultSelected: PropTypes.string
+};
+
+export default BlogCategoryBar;
--- src/pages/alkemy-blog.js
+++ src/pages/alkemy-blog.js
@@ -3,7 +3,7 @@ import {uniq,isEqual,sortBy} from "lodash";
 import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 import { Context } from "../store/appContext.js";
-import { addJS, fluidImageSmall } from "../utils/utils.js";
+import { fluidImageSmall } from "../utils/utils.js";
 import Layout from "../components/layout";
 import ScrollWrapper from "../components/scrollWrapper.jsx";
 import { Button, Col, Row, Label, FormGroup } from "reactstrap";
@@ -14,8 +14,7 @@ import BlogInfoBar from "../components/BlogInfoBar.jsx";
 import RecentBlogs from "../components/RecentBlogs.jsx";
 import LatestFromCategory from "../components/LatestFromCategory.jsx";
 import PropTypes from "prop-types";
-
-
+import BlogCategoryBar from "../components/BlogCategoryBar.jsx";
 
 /*
 Layout props:
@@ -41,46 +40,22 @@ const AlkemyBlog = ({
     const pageTitle = { name: "Alkemy Blog", url: "/alkemy-blog" };
 
     // define state hooks
-    const [dropdown, setDropdown] = useState("");
+    const [category, setCategory] = useState("featured");
     const [filterBySearch, setFilter] = useState(false);
     const [searchResults, setSearchResults] = useState(0);
 
-    // useEffect hook to check if their is a state value and trigger it in the dropdown
+    // useEffect hook to check if there is a state value and trigger it in the dropdown
     useEffect(() => {
         if (location.state && location.state.value) {
             // use location.state to get information from single post
             let cat = location.state.value;
 
             // set the dropdown parameters and reset the search
-            setDropdown(cat);
+            setCategory(cat);
         }
     }, []); // pass empty array as second arg so it only runs on mount
 
-    // addJS(position,inner script,source) - adds JS to document dynamically for AddThis Toolbar
-    // addJS(
-    //     `body`,
-    //     null,
-    //     `//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ae21853f0b21631`,
-    //     `addThis`
-    // );
-
-    let createBlogArray = (arr, home = true) => {
-        let blogArray = arr.map(e => e);
-        if (home && blogArray.length > 0) blogArray.shift();
-        return blogArray;
-    };
-
-    const resetDropdown = () => {
-        setDropdown("");
-    };
-    const resetSearch = actions => {
-        setFilter(false);
-        setSearchResults(0);
-        actions.search("");
-        actions.searchTitle("");
-    };
-
-    const blogCategories = (jump = false) => {
+    const blogCategories = () => {
         // create a categories array
         let categories =
             edges &&
@@ -93,152 +68,44 @@ const AlkemyBlog = ({
         let categoryArray = [];
 
         // if it's the Jump to menu, push in a home case
-        jump && categoryArray.push({ label: "Blog Home", value: "Blog Home" });
+        categoryArray.push({ label: "Featured", value: "featured" });
 
         // loop the category array and push in pairs to aux
         for (let i in categories) {
-            categoryArray.push({ label: categories[i], value: categories[i] });
+            categoryArray.push({
+                label: categories[i],
+                value: categories[i].toLowerCase(),
+            });
         }
 
         return categoryArray;
     };
 
-    const handleDropdownChange = (value, actions) => {
-        setDropdown(value);
-        resetSearch(actions);
+    let createBlogArray = (arr, home = true) => {
+        let blogArray = arr.map(e => e);
+        if (home && blogArray.length > 0) blogArray.shift();
+        return blogArray;
     };
 
-    const renderView = store => {
-        let blogs = edges.map(e => e);
-
-        if (store.searchResults.length > 0) {
-            let results = store.searchResults;
-            blogs = blogs.filter(e => {
-                for (let item in results) {
-                    if (results[item].path === e.node.frontmatter.path)
-                        return e;
-                }
-            });
-
-            setFilter(true);
-            setSearchResults(blogs.length);
-            resetDropdown();
-        } else if (
-            dropdown.value &&
-            dropdown.value.length > 0 &&
-            dropdown.value.toLowerCase() != "blog home"
-        ) {
-            blogs = blogs.filter(e => {
-                return e.node.frontmatter.category === dropdown.value;
-            });
-
-            setFilter(false);
-            setSearchResults(0);
-        }
-
-        if (filterBySearch === false) {
-            return (
-                <>
-                    {isEqual(sortBy(blogs), sortBy(edges)) ? (
-                        <section className="blog-featured position-relative alk-container">
-                            <Row className="h-100 align-items-center">
-                                <Col xs={12} lg={6} className="h-100">
-                                    <h2>
-                                        {blogs &&
-                                            blogs[0].node.frontmatter.title}
-                                    </h2>
-                                    <p className="my-4">
-                                        {blogs &&
-                                            blogs[0].node.frontmatter.excerpt}
-                                    </p>
-                                    <BlogInfoBar
-                                        category={
-                                            blogs[0].node.frontmatter.category
-                                        }
-                                        time={
-                                            blogs[0].node.frontmatter
-                                                .readingTime
-                                        }
-                                        author={
-                                            blogs[0].node.frontmatter.author
-                                        }
-                                        layout="horizontal"
-                                        className="my-4"
-                                    />
-                                    <Button
-                                        to={blogs[0].node.frontmatter.path}
-                                        tag={Link}
-                                        color="primary"
-                                        className="my-4"
-                                        block
-                                    >
-                                        Read Full Post
-                                    </Button>
-                                </Col>
-                                <Col
-                                    xs={12}
-                                    lg={6}
-                                    className="mb-5 mb-lg-0 order-first order-lg-last"
-                                >
-                                    <Img
-                                        className="h-100 featured-blog-cover-image"
-                                        fluid={
-                                            blogs[0].node.frontmatter.cover
-                                                .childImageSharp.fluid
-                                        }
-                                        alt={blogs[0].node.frontmatter.coverAlt}
-                                    />
-                                </Col>
-                            </Row>
-                        </section>
-                    ) : null}
-
-                    {isEqual(sortBy(blogs), sortBy(edges)) ? (
-                        <section className="py-4 blog-post-listing alk-container">
-                            <Row>
-                                <Col xs={12} lg={9}>
-                                    <RecentBlogs
-                                        className=""
-                                        blogdata={
-                                            blogs && createBlogArray(blogs)
-                                        }
-                                        layout="home"
-                                    />
-                                </Col>
-                                <Col lg={3} className="d-none d-lg-block">
-                                    <LatestFromCategory
-                                        blogdata={blogs && blogs}
-                                        categories={blogs && blogCategories()}
-                                    />
-                                </Col>
-                            </Row>
-                        </section>
-                    ) : (
-                        <section className="py-4 blog-post-listing alk-container">
-                            <Row>
-                                <Col xs={12}>
-                                    <RecentBlogs
-                                        blogdata={createBlogArray(blogs, false)}
-                                        layout="alt"
-                                    />
-                                </Col>
-                            </Row>
-                        </section>
-                    )}
-                </>
-            );
-        } else {
-            return (
-                <section className="py-4 blog-post-listing alk-container">
-                    <Row>
-                        <Col xs={12}>
-                            <RecentBlogs blogdata={blogs} layout="alt" />
-                        </Col>
-                    </Row>
-                </section>
-            );
-        }
+    const resetSearch = actions => {
+        setFilter(false);
+        setSearchResults(0);
+        actions.search("");
+        actions.searchTitle("");
     };
+
+    const renderView = ()=>{
+        return (
+            <Row>
+                <Col xs={6}>
+                    test
+                </Col>
+                <Col xs={6}>
+                    test2
+                </Col>
+            </Row>
+        )
+    }
 
     return (
         <ScrollWrapper onWindowScroll={handleScroll}>
@@ -249,59 +116,15 @@ const AlkemyBlog = ({
                 bodyClasses="blog"
             >
                 <SEO title={pageTitle.name} />
-                <Context.Consumer>
-                    {({ actions }) => {
-                        return (
-                            <section className="blog-category-filter my-3">
-                                <Row className="align-items-center h-100 alk-container">
-                                    <Col 
-                                        xs={12} 
-                                        sm={{size: 10,offset:2}} 
-                                        md={{size:8,offset:4}} 
-                                        lg={{size:6,offset:6}} 
-                                        xl={{size:4,offset:8}} 
-                                        >
-
-                                        {/* Category Dropdown */}
-                                        <FormGroup
-                                            row
-                                            className="align-items-center"
-                                        >
-                                            <Label
-                                                for="categories"
-                                                xs={4}
-                                                className="text-right text-muted"
-                                            >
-                                                Jump to:
-                                            </Label>
-                                            <Col 
-                                                xs={8}
-                                                >
-                                                <Select
-                                                    className="category-select"
-                                                    classNamePrefix="select"
-                                                    placeholder="Select a value..."
-                                                    name="categories"
-                                                    options={blogCategories(
-                                                        true
-                                                    )}
-                                                    ref={categorySelect}
-                                                    value={dropdown}
-                                                    onChange={value =>
-                                                        handleDropdownChange(
-                                                            value,
-                                                            actions
-                                                        )
-                                                    }
-                                                />
-                                            </Col>
-                                        </FormGroup>
-                                    </Col>
-                                </Row>
-                            </section>
-                        );
-                    }}
-                </Context.Consumer>
+                <Row>
+                    <Col xs={12}>
+                        <BlogCategoryBar
+                            defaultSelected={category}
+                            categories={blogCategories()}
+                            onSelectCategory={setCategory}
+                        />
+                    </Col>
+                </Row>
 
                 <Context.Consumer>
                     {({ store }) => {
