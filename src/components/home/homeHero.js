22403390cb5ab0bae8a958fe0b29c699ffb42909
jferragut <31458614+jferragut@users.noreply.github.com>
2021-02-24 14:25:34 -0500

    changed hero to gatsby img from gatsby background img

--- src/components/home/homeHero.js
+++ src/components/home/homeHero.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import BackgroundImage from "gatsby-background-image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
 import { Button, Col, Row } from "reactstrap";
@@ -26,81 +25,82 @@ const HomeHero = (props)=>{
 
     return (
         <section className="hero-wrapper bg-black">
-            <BackgroundImage
-                className="homeHero d-flex flex-column align-items-center justify-content-lg-center h-100"
+            <Img
+                style={{
+                    position: "absolute",
+                }}
+                className="homeHero h-100"
                 fluid={heroImg}
                 alt="view of tall skyscrapers while looking up from the ground"
-            >
-                <div className="container-fluid p-0 h-100 position-relative d-flex flex-column justify-content-lg-center">
-                    <Row className="d-block d-lg-none">
+            />
+            
+            <div className="container-fluid p-0 h-100 position-relative d-flex flex-column justify-content-lg-center">
+                <Row className="d-block d-lg-none">
+                    {data.alkemyStack.childImageSharp && (
+                        <Img
+                            imgStyle={{
+                                objectFit: "contain",
+                                padding: "0 1rem",
+                            }}
+                            style={{
+                                maxWidth: "580px",
+                            }}
+                            className="mx-auto"
+                            objectFit="contain"
+                            fluid={data.alkemyStack.childImageSharp.fluid}
+                            alt="Logos of various programming languages and frameworks we use"
+                        />
+                    )}
+                </Row>
+                <Row className="cover-text-row d-flex align-items-center w-100 no-gutters">
+                    <Col xs={12} lg={7}>
+                        <div className="cover-text">
+                            <div className="mb-3 mb-lg-5">
+                                <h1 className="hero-heading d-block mb-3">
+                                    Unique Digital Experiences
+                                </h1>
+                                <p className="d-block h5 font-weight-normal">
+                                    Expertly designed and crafted to wow your
+                                    customers and increase sales.
+                                </p>
+                            </div>
+
+                            {/* Cover CTA */}
+                            <Button
+                                color="primary"
+                                size="lg"
+                                to="/project-enquiry"
+                                tag={Link}
+                            >
+                                Start your project
+                            </Button>
+                        </div>
+                    </Col>
+                    <Col lg={5} className="d-none d-lg-block pl-md-4">
                         {data.alkemyStack.childImageSharp && (
                             <Img
-                                imgStyle={{
-                                    objectFit: "contain",
-                                    padding: "0 1rem",
-                                }}
-                                style={{
-                                    maxWidth: "580px",
-                                }}
-                                className="mx-auto"
-                                objectFit="contain"
                                 fluid={data.alkemyStack.childImageSharp.fluid}
                                 alt="Logos of various programming languages and frameworks we use"
                             />
                         )}
-                    </Row>
-                    <Row className="cover-text-row d-flex align-items-center w-100 no-gutters">
-                        <Col xs={12} lg={7}>
-                            <div className="cover-text">
-                                <div className="mb-3 mb-lg-5">
-                                    <h1 className="hero-heading d-block mb-3">
-                                        Unique Digital Experiences
-                                    </h1>
-                                    <p className="d-block h5 font-weight-normal">
-                                        Expertly designed and crafted to wow
-                                        your customers and increase sales.
-                                    </p>
-                                </div>
-
-                                {/* Cover CTA */}
-                                <Button
-                                    color="primary"
-                                    size="lg"
-                                    to="/project-enquiry"
-                                    tag={Link}
-                                >
-                                    Start your project
-                                </Button>
-                            </div>
-                        </Col>
-                        <Col lg={5} className="d-none d-lg-block pl-md-4">
-                            {data.alkemyStack.childImageSharp && (
-                                <Img
-                                    fluid={
-                                        data.alkemyStack.childImageSharp.fluid
-                                    }
-                                    alt="Logos of various programming languages and frameworks we use"
-                                />
-                            )}
-                        </Col>
-                    </Row>
-                </div>
-                {/* Caret */}
-                <motion.div
-                    variants={card}
-                    initial="rest"
-                    whileHover="hover"
-                    whileTap="pressed"
-                    className="heroChevron"
-                >
-                    <FontAwesomeIcon
-                        onClick={props.handleCaretClick}
-                        icon="chevron-down"
-                        size="3x"
-                        color="white"
-                    />
-                </motion.div>
-            </BackgroundImage>
+                    </Col>
+                </Row>
+            </div>
+            {/* Caret */}
+            <motion.div
+                variants={card}
+                initial="rest"
+                whileHover="hover"
+                whileTap="pressed"
+                className="heroChevron"
+            >
+                <FontAwesomeIcon
+                    onClick={props.handleCaretClick}
+                    icon="chevron-down"
+                    size="3x"
+                    color="white"
+                />
+            </motion.div>
         </section>
     );
 }
